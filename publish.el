a60687c0b1fa2810cb0fccf6ce3e4e2d984d9979
josiah <me@jowj.net>
2022-11-06 11:57:04 -0600

    Noisy commit - sorry for RSS subscribers?
    - Add category tag to each file
    - Add new code from Ambrevar, anonimno, to help with date management
    - in articles.org
    - regenerate site.

--- publish.el
+++ publish.el
@@ -57,9 +57,19 @@
   :ensure t)
 
 ;;;; custom var/func defs to deal with frustrating org limitations
-(setq make-backup-files nil)
-(setq org-html-postamble t
-      org-html-validation-link nil)
+(setq make-backup-files nil
+      org-html-postamble t
+      org-html-validation-link nil
+      org-export-with-date t
+      org-publish-use-timestamps-flag t
+      org-publish-timestamp-directory "./"
+      org-export-with-section-numbers nil
+      org-export-with-email nil
+      org-export-with-tags 'not-in-toc
+      org-export-with-toc nil
+      org-html-metadata-timestamp-format "%Y-%m-%d"
+      org-html-checkbox-type 'html)
+
 
 (defvar org-blog-date-format "%h %d, %Y"
   "Format for displaying publish dates.")
@@ -68,7 +78,62 @@
 (defvar org-blog-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/my-dark.css\"/>")
 (defvar org-personal-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"../posts/css/my-dark.css\"/>")
 (defvar org-index-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"posts/css/my-dark.css\"/>")
+(defvar ambrevar/repository "https://github.com/jowj/jowj.github.io")
+(defvar ambrevar/root (expand-file-name "."))
 
+
+(defun ambrevar/git-creation-date (file)
+  "Return the first commit date of FILE.
+Format is %Y-%m-%d."
+  (with-temp-buffer
+    (call-process "git" nil t nil "log" "--reverse" "--date=short" "--pretty=format:%cd" file)
+    (goto-char (point-min))
+    (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
+
+(defun ambrevar/git-last-update-date (file)
+  "Return the last commit date of FILE.
+Format is %Y-%m-%d."
+  (with-output-to-string
+    (with-current-buffer standard-output
+      (call-process "git" nil t nil "log" "-1" "--date=short" "--pretty=format:%cd" file))))
+
+(defun ambrevar/org-html-format-spec (info)
+  "Return format specification for preamble and postamble.
+INFO is a plist used as a communication channel.
+Just like `org-html-format-spec' but uses git to return creation and last update
+dates.
+The extra `u` specifier displays the creation date along with the last update
+date only if they differ."
+  (let* ((timestamp-format (plist-get info :html-metadata-timestamp-format))
+         (file (plist-get info :input-file))
+         (meta-date (org-export-data (org-export-get-date info timestamp-format)
+                                     info))
+         (creation-date (if (string= "" meta-date)
+                            (ambrevar/git-creation-date file)
+                          ;; Default to the #+DATE value when specified.  This
+                          ;; can be useful, for instance, when Git gets the file
+                          ;; creation date wrong if the file was renamed.
+                          meta-date))
+         (last-update-date (ambrevar/git-last-update-date file)))
+    `((?t . ,(org-export-data (plist-get info :title) info))
+      (?s . ,(org-export-data (plist-get info :subtitle) info))
+      (?d . ,creation-date)
+      (?T . ,(format-time-string timestamp-format))
+      (?a . ,(org-export-data (plist-get info :author) info))
+      (?e . ,(mapconcat
+	      (lambda (e) (format "<a href=\"mailto:%s\">%s</a>" e e))
+	      (split-string (plist-get info :email)  ",+ *")
+	      ", "))
+      (?c . ,(plist-get info :creator))
+      (?C . ,last-update-date)
+      (?v . ,(or (plist-get info :html-validation-link) ""))
+      (?u . ,(if (string= creation-date last-update-date)
+                 creation-date
+               (format "%s (<a href=%s>Last update: %s</a>)"
+                       creation-date
+                       (format "%s/commits/master/%s" ambrevar/repository (file-relative-name file ambrevar/root))
+                       last-update-date))))))
+(advice-add 'org-html-format-spec :override 'ambrevar/org-html-format-spec)
 
 (defun jlj/preamble (info)
   "Return preamble as a string.  INFO."
@@ -77,13 +142,32 @@
          (prefix (file-relative-name (expand-file-name jlj/root)
                                      (file-name-directory file))))
     (format
-     "<a href=\"%1$s/index.html\">home</a>
+     "<nav>
+<a href=\"%1$s/index.html\">home</a>
 <a href=\"%1$s/posts/articles.html\">essays</a>
 <a href=\"%1$s/personal/articles.html\">diary</a>
 <a href=\"https://git.awful.club/projects\">projects</a>
 <a href=\"%1$s/resume.pdf\">resume</a>
-<a href=\"%1$s/lore.html\">lore</a>"
+<a href=\"%1$s/lore.html\">lore</a>
+</nav>"
      prefix)))
+
+
+(defun my-site-format-entry (entry style project)
+  (format "%s - [[file:%s][%s]] (%s)"
+          (format-time-string "%Y-%m-%d" (org-publish-find-date entry project))
+          entry
+          (org-publish-find-title entry project)
+          (org-find-category (expand-file-name entry (plist-get (cdr project) :base-directory)))))
+
+(defun org-find-category (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (goto-char (point-min))
+    (let ((beg (+ 1 (re-search-forward "^#\\+CATEGORY\:")))
+          (end (progn (forward-word) (point))))
+      (buffer-substring beg end))))
+
 
 (setq org-html-postamble-format `(("en" ,(concat "<p class=\"creator\">Made with %c</p></p>"))))
 (setq org-html-preamble #'jlj/preamble)
@@ -95,12 +179,14 @@
              :recursive t
 	     :exclude "index.org"
              :publishing-function '(org-html-publish-to-html)
+	     :time-stamp-file nil
              :publishing-directory "./posts/"
 	     :sitemap-title "a list of stuff i wrote"
              :sitemap-filename "articles.org"
              :auto-sitemap t
              :sitemap-style 'list
              :sitemap-sort-files 'anti-chronologically
+	     :sitemap-format-entry 'my-site-format-entry
 	     :html-html5-fancy t
 	     :html-doctype "html5"
              :html-head-include-default-style nil
@@ -116,6 +202,8 @@
              :recursive t
 	     :exclude "index.org"
              :publishing-function '(org-html-publish-to-html)
+	     :sitemap-format-entry 'my-site-format-entry
+	     :time-stamp-file t
              :publishing-directory "./personal/"
 	     :sitemap-title "personal bullshit"
              :sitemap-filename "articles.org"
@@ -130,6 +218,7 @@
              :exclude "\\.org\\'"
              :publishing-directory "."
              :publishing-function 'org-publish-attachment
+	     :time-stamp-file t
              :recursive t)
        (list "site" :components '("site-org"))))
 
